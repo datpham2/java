de111dbbfa9fc0f8cfc7e4bb8b209ccd4d8a1e73
datpham2 <datpham3141@gmail.com>
2024-08-20 16:53:20 +0700

    added first Java project

---
+++ buoi1.1/src/HinhChuNhat.java
@@ -0,0 +1,13 @@
+public class HinhChuNhat {
+    public float dai;
+    public float rong;
+
+    public HinhChuNhat() {
+
+    }
+
+    public HinhChuNhat(float d, float r) {
+        this.dai = d;
+        this.rong = r;
+    }
+}
